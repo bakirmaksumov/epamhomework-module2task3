521a62d6377eb489cc6bad722666262358984f48
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-07-12 11:18:30 +0500

    initial

---
+++ HelloAppConsoleWithLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyLibrary;
+
+namespace HelloAppConsoleWithLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Здравствуйте введите свое имя:");
+            string name = Console.ReadLine();
+            var lib = new OutputInfo(); 
+            Console.WriteLine(lib.inputInfo(name));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyLibrary/OutputInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class OutputInfo
+    {
+        public string inputInfo(string name)
+        {
+            string _name = name;
+            string result = String.Empty;
+            if (_name != String.Empty)
+            {
+                result = $"{DateTime.Now.ToString("h:mm:ss tt")}, Hello {_name}";
+                return result;
+            }
+            else
+            {
+                result = "No name, please input the name";
+                return result;
+            }
+        }
+    }
+}
